16a197e4206358663f352a28d0a6ea71aa337eac
leftie friele <leftie.friele@gmail.com>
2011-10-13 18:37:05 +0200

    prematurely adding some text

---
+++ appConfig.js
@@ -0,0 +1,11 @@
+exports.config = {
+	databaseHost: "",
+	databaseName: "",
+	databasePort: 27017,
+	webappPort: 29099,
+	twitterUsername: "<twitter account>",
+	twitterPassword: "<twitter passord>",
+	trackArgument: "",
+	trackItems: ["some", "words"],
+	follow: [16738187, 22913112, 87416923]
+};
